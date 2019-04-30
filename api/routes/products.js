40c5c08599cbd31493189593dad51dc474af8c70
cio <cio@mitlab.kr>
2019-04-30 15:57:00 +0900

    body-parser 통한 products 파일의 router POST 구현

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+
+
+ 
 // products 파일의 router get 구현
 router.get("/", (req, res) => {
     res.status(200).json({
@@ -8,15 +11,24 @@ router.get("/", (req, res) => {
     });
 });
 
+// body-parser 통한 products 파일의 router POST 구현
+router.post("/", (req , res) => {
+    const product = {
+        // 단순 입력 데이터 출력이라면 당연히 params(파라메터)로
+        name: req.param.name,
+        price : req.params.price
+        
+        // 디비에 넣으려면 body
+        // name : req.body.name,
+        // price : req.body.price
+
+    };    
  
- // POST router
- router.post("/", (req, res) => {
-     res.status(200).json({
-         message : "post /Products"
-     });
- 
+    res.status(200).json({
+        message : "POST /Products"
+    });
  });
- 
+
  // PUT router
  router.put("/", (req, res) => {
      res.status(200).json({
--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodypaser = require("body-parser");
-const PORT = 3000;
 
-//Start Server
-const server = http.createServer(app);
-server.listen(PORT, console.log("서버 시작!!!"));
+
 
 // 하위 라우터들의 정의
 const orderRoutes = require("./api/routes/orders");
@@ -17,36 +14,38 @@ const productRoutes = require("./api/routes/products");
 app.use("/orders", orderRoutes);
 app.use("/products", productRoutes);
 
+// 에러처리를 위한 MORGAN 객체 생성, 에러메시지처리 부분 구현
+app.use(morgan("dev"));
+
 //body를 다루기 위해 body-parser 객체 생성
-app.use(bodypaser.json());
-app.use(bodypaser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-    if(req.method === "OPTION"){
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
     next();
 });
 
-// server 파일의 get 구현
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message : "It's a root!!!"
-    });
- });
- 
-// 에러처리를 위한 MORGAN 객체 생성, 에러메시지처리 부분 구현
-app.use(morgan("dev"));
+
+// // server 파일의 get 구현
+// app.get("/", (req, res) => {
+//     res.status(200).json({
+//         message : "It's a root!!!"
+//     });
+//  });
+//
+
 app.use((req, res, next) => {
     const error = new Error("요청 파일 못차즘");
     error.status = 404;
     next(error);
 });
+
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
@@ -56,4 +55,8 @@ app.use((error, req, res, next) => {
     });
 });
 
-// body-parser 적용
+const PORT = 3000;
+
+//Start Server
+const server = http.createServer(app);
+server.listen(PORT, console.log("서버 시작!!!"));
